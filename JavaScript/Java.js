95681f23f02ff9dc4df1a8ae42ffbce0870367d6
DevaNnathan <v.devanathan222@gmail.com>
2025-09-04 15:58:26 +0530

    Operators

---
+++ JavaScript/Java.js
@@ -0,0 +1,30 @@
+
+let six;
+
+  let a= 6;
+  if (a % 2 ===0)
+    {
+    six = "Even number";
+    console.log('Six is '+ six);
+    } 
+    else 
+    {
+    six = "Odd number";
+    console.log('Six is ' + six);
+    }
+
+let fifty;
+
+   let d = 5;
+    if (d % 5 === 0) {
+    fifty = "divisible by 5";
+    console.log('Fifty is '+ fifty);
+    } 
+    else {
+    fifty = "not divisible by 5";
+    console.log('Fifty is ' + fifty);
+    }
+let bigger
+
+   let b = 50;
+   if (b )
